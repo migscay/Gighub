bedf0d7efd202fe824e3cab5fa8d3995a4ffb49f
migscay <popeye.santiago@gmail.com>
2022-06-28 09:44:25 +1000

    enable client-side validations

--- Gighub/ViewModels/GigFormViewModel.cs
+++ Gighub/ViewModels/GigFormViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Gighub.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,17 +9,25 @@ namespace Gighub.ViewModels
 {
     public class GigFormViewModel
     {
+        [Required]
         public string Venue { get; set; }
+
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+        
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
+
+        [Required]
         public byte Genre { get; set; }
+        
         public IEnumerable<Genre> Genres{ get; set; }
-        public DateTime DateTime 
-        {          
-            get
-            {
-               return DateTime.Parse(string.Format("{0} {1}", Date, Time));
-            }                
+
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
         }
     }
 }
