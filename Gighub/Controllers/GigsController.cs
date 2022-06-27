e9ffa4f6181a929c63a114f17a0f1aa9108c14c7
migscay <popeye.santiago@gmail.com>
2022-06-28 06:32:03 +1000

    Implement saving a Gig

--- Gighub/Controllers/GigsController.cs
+++ Gighub/Controllers/GigsController.cs
@@ -1,5 +1,7 @@
 ﻿using Gighub.Models;
 using Gighub.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -7,12 +9,13 @@ namespace Gighub.Controllers
 {
     public class GigsController : Controller
     {
-        // GET: Gigs
-        private ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
         public GigsController()
         {
             _context = new ApplicationDbContext();
         }
+
+        [Authorize]
         public ActionResult Create()
         {
             var ViewModel = new GigFormViewModel
@@ -21,5 +24,25 @@ namespace Gighub.Controllers
             };
             return View(ViewModel);
         }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+
+            var gig = new Gig
+            {
+                ArtistId = User.Identity.GetUserId(),
+                DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
+            };
+            
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+
     }
 }
