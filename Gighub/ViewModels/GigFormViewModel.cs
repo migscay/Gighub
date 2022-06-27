9604e369ce6d7213b36025f2ba803b1ecf048f12
migscay <popeye.santiago@gmail.com>
2022-06-28 06:52:06 +1000

    Move DataTime parsing to viewModel

--- Gighub/ViewModels/GigFormViewModel.cs
+++ Gighub/ViewModels/GigFormViewModel.cs
@@ -13,5 +13,12 @@ namespace Gighub.ViewModels
         public string Time { get; set; }
         public byte Genre { get; set; }
         public IEnumerable<Genre> Genres{ get; set; }
+        public DateTime DateTime 
+        {          
+            get
+            {
+               return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+            }                
+        }
     }
 }
