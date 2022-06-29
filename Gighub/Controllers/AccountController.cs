ba0e02affc982ced9622bd6dce6cb524bfb6127c
migscay <popeye.santiago@gmail.com>
2022-06-29 18:36:15 +1000

    Added Name in Signup

--- Gighub/Controllers/AccountController.cs
+++ Gighub/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Gighub.Models;
+using Gighub.ViewModels;
 
 namespace Gighub.Controllers
 {
@@ -151,7 +152,10 @@ namespace Gighub.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser 
+                    { UserName = model.Email, 
+                        Email = model.Email, 
+                        Name = model.Name };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
--- Gighub/Controllers/HomeController.cs
+++ Gighub/Controllers/HomeController.cs
@@ -1,16 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Gighub.Models;
+using System.Data.Entity;
 using System.Web.Mvc;
+using System.Linq;
 
 namespace Gighub.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();        
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var upcomingGigs = _context.Gigs
+                .Include(g => g.Artist)
+                .Where(g => g.DateTime > System.DateTime.Now);
+            return View(upcomingGigs);
         }
 
         public ActionResult About()
